5080a25985212ee0a612779c620bd32f589d9284
Mohammed Roshan <coolron90@gmail.com>
2014-12-23 18:50:39 +0530

    Revised Coin Production

--- js/util.bitcoin.js
+++ js/util.bitcoin.js
@@ -138,7 +138,7 @@ function testnetBurnDetermineEarned(blockHeight, burned) {
   burned = denormalizeQuantity(burned);
   var total_time = TESTNET_BURN_END - TESTNET_BURN_START;
   var partial_time = TESTNET_BURN_END - blockHeight;
-  var multiplier = 100 * (0.85 + 0.15 * (partial_time / total_time)); //will be approximate
+  var multiplier = 1 * (0.7 + 0.3 * (partial_time / total_time)); //will be approximate
   var earned = Decimal.round(new Decimal(burned).mul(multiplier), 8, Decimal.MidpointRounding.ToEven).toFloat();
   return normalizeQuantity(earned);
 }
